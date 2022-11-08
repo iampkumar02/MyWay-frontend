2a81788368140f16a10419cdd0ef3410cf354e3a
iampkumar02 <pkjjjjj123@gmail.com>
2022-11-09 00:14:37 +0530

    myway

---
+++ index.js
@@ -0,0 +1,35 @@
+const apiUrl = "http://localhost:8000";
+
+const submitForm = document.querySelector(".form");
+
+submitForm.addEventListener("submit", (event) => {
+    event.preventDefault();
+    const sendquery = document.querySelector("#query");
+    const sendemail = document.querySelector("#sendemail");
+
+    const query = sendquery.value;
+    const email = sendemail.value;
+
+    fetch(`${apiUrl}/sendquery`, {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ query: query, email: email }),
+    })
+        .then((res) => res.json())
+        .then((data) => {
+            console.log(data);
+            const { token } = data;
+            if (token) {
+                localStorage.setItem("jwt", token);
+                location.href = "index.html";
+            } else {
+                alert("Send Again");
+            }
+        })
+        .catch((err) => {
+            alert("Error while sending query.... try again!");
+            console.log(err);
+        })
+})
